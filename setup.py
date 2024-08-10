6a5932ac0a8f1770b6acf2dbd00b3db76dd71006
kimeloo <iroo200121@gmail.com>
2024-08-11 01:53:47 +0900

    Update : installed pip modules
    requests==2.32.3
    discord==2.3.2
    python-dotenv==1.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CreateThread',
+    version='0.1',
+    install_requires=[
+        'requests==2.32.3',
+        'discord==2.3.2',
+        'python-dotenv==1.0.1',
+    ],
+    python_requires='>=3.8'
+)
