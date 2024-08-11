fbe71db22de710ce02a3ca40a8d897330e1048a4
kimeloo <iroo200121@gmail.com>
2024-08-11 19:39:10 +0900

    Refactor : bot.py 내 commands, events 분리

--- discord_automation/bot.py
+++ discord_automation/bot.py
@@ -1,15 +1,15 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands as discord_commands
 import os
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
+from .commands import Commands as mycommands
+from .events import Events as myevents
 import logging
 logger = logging.getLogger("bot")
 
 class ThreadBot():
     def __init__(self):
         self.__TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-        self.__CHANNEL_ID_discussion = os.getenv('CHANNEL_DISCUSSION')
+        # self.__CHANNEL_ID_discussion = os.getenv('CHANNEL_DISCUSSION')
         self.__create_bot()
     
     def __create_bot(self):
@@ -18,56 +18,56 @@ class ThreadBot():
         intents.guilds = True
         intents.reactions = True
         
-        bot = commands.Bot(command_prefix='!', intents=intents)
-        bot = self.__add_events(bot)
-        bot = self.__add_commands(bot)
+        bot = discord_commands.Bot(command_prefix='!', intents=intents)
+        bot = myevents(bot).add()
+        bot = mycommands(bot).add()
         self.bot = bot
     
-    def __add_events(self, bot):
-        @bot.event
-        async def on_ready():
-            logger.info(f'Logged in as {bot.user.name}')
+    # def __add_events(self, bot):
+    #     @bot.event
+    #     async def on_ready():
+    #         logger.info(f'Logged in as {bot.user.name}')
         
-        @bot.event
-        async def on_raw_reaction_add(reaction):
-            logger.debug("EVENT")
-            logger.debug(reaction.message)
-            logger.debug(reaction.message.content)
-            logger.debug(reaction.message.channel.parent)
-            if (reaction.message.channel.name == 'idea') or (reaction.message.channel.parent.name == 'idea'):
-                discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
-                logger.debug(f'Get Channel by ID {self.__CHANNEL_ID_discussion}')
-                new_message = await discussion_channel.send(reaction.message.content)
-                await new_message.create_thread(name=reaction.message.content)
+    #     @bot.event
+    #     async def on_raw_reaction_add(reaction):
+    #         logger.debug("EVENT")
+    #         logger.debug(reaction.message)
+    #         logger.debug(reaction.message.content)
+    #         logger.debug(reaction.message.channel.parent)
+    #         if (reaction.message.channel.name == 'idea') or (reaction.message.channel.parent.name == 'idea'):
+    #             discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
+    #             logger.debug(f'Get Channel by ID {self.__CHANNEL_ID_discussion}')
+    #             new_message = await discussion_channel.send(reaction.message.content)
+    #             await new_message.create_thread(name=reaction.message.content)
 
-        @bot.event
-        async def on_raw_reaction_add(payload):
-            channel = await self.bot.fetch_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
-            emoji = payload.emoji
-            logger.info(emoji.name)
-            if (channel.name == 'idea') or (channel.parent.name == 'idea'):
-                if emoji.name == "✅":
-                    discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
-                    new_message = await discussion_channel.send(message.content)
-                    thread = await new_message.create_thread(name=message.content, auto_archive_duration=10080)
+    #     @bot.event
+    #     async def on_raw_reaction_add(payload):
+    #         channel = await self.bot.fetch_channel(payload.channel_id)
+    #         message = await channel.fetch_message(payload.message_id)
+    #         emoji = payload.emoji
+    #         logger.info(emoji.name)
+    #         if (channel.name == 'idea') or (channel.parent.name == 'idea'):
+    #             if emoji.name == "✅":
+    #                 discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
+    #                 new_message = await discussion_channel.send(message.content)
+    #                 thread = await new_message.create_thread(name=message.content, auto_archive_duration=10080)
 
-                    seoul_tz = ZoneInfo('Asia/Seoul')
-                    creation_time = datetime.now(seoul_tz)
-                    archive_time = creation_time + timedelta(days=7)
-                    creation_time = creation_time.strftime("%Y-%m-%d %H:%M:%S")
-                    archive_time = archive_time.strftime("%Y-%m-%d %H:%M:%S")
-                    await thread.send(f"@everyone\n Created : {creation_time} (UTC+9)\nArchived : {archive_time} (UTC+9).")
-        return bot
+    #                 seoul_tz = ZoneInfo('Asia/Seoul')
+    #                 creation_time = datetime.now(seoul_tz)
+    #                 archive_time = creation_time + timedelta(days=7)
+    #                 creation_time = creation_time.strftime("%Y-%m-%d %H:%M:%S")
+    #                 archive_time = archive_time.strftime("%Y-%m-%d %H:%M:%S")
+    #                 await thread.send(f"@everyone\n Created : {creation_time} (UTC+9)\nArchived : {archive_time} (UTC+9).")
+    #     return bot
 
-    def __add_commands(self, bot):
-        @bot.command()
-        async def ping(ctx):
-            send_message = 'pong'
-            logger.info("[{:>8}] : {}".format("Recieved", ctx.message.content))
-            await ctx.channel.send(send_message)
-            logger.info("[{:>8}] : {}".format("Sent", send_message))
-        return bot
+    # def __add_commands(self, bot):
+    #     @bot.command()
+    #     async def ping(ctx):
+    #         send_message = 'pong'
+    #         logger.info("[{:>8}] : {}".format("Recieved", ctx.message.content))
+    #         await ctx.channel.send(send_message)
+    #         logger.info("[{:>8}] : {}".format("Sent", send_message))
+    #     return bot
     
     def run(self):
         if self.__TOKEN:
---
+++ discord_automation/events.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import os
+import logging
+logger = logging.getLogger("event")
+
+class Events():
+    def __init__(self, bot):
+        self.__CHANNEL_ID_discussion = os.getenv('CHANNEL_DISCUSSION')
+        self.bot = bot
+    
+    def add(self):
+        return self.__events(self.bot)
+
+    def __events(self, bot):
+        @bot.event
+        async def on_ready():
+            logger.info(f'Logged in as {bot.user.name}')
+        
+        # @bot.event
+        # async def on_raw_reaction_add(reaction):
+        #     logger.info(reaction.message.content)
+        #     logger.debug(reaction.message.content)
+        #     logger.debug(reaction.message.channel.parent)
+        #     if (reaction.message.channel.name == 'idea') or (reaction.message.channel.parent.name == 'idea'):
+        #         discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
+        #         logger.debug(f'Get Channel by ID {self.__CHANNEL_ID_discussion}')
+        #         new_message = await discussion_channel.send(reaction.message.content)
+        #         await new_message.create_thread(name=reaction.message.content)
+
+        @bot.event
+        async def on_raw_reaction_add(payload):
+            channel = await bot.fetch_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            emoji = payload.emoji
+            if (channel.name == 'idea') or (channel.parent.name == 'idea'):
+                if emoji.name == "✅":
+                    logger.info(f'Reaction {emoji.name} on [{message.content}]')
+                    discussion_channel = bot.get_channel(int(self.__CHANNEL_ID_discussion))
+                    new_message = await discussion_channel.send(message.content)
+                    thread = await new_message.create_thread(name=message.content, auto_archive_duration=10080)
+                    logger.info(f'Created [{message.content}] thread.')
+
+                    seoul_tz = ZoneInfo('Asia/Seoul')
+                    creation_time = datetime.now(seoul_tz)
+                    archive_time = creation_time + timedelta(days=7)
+                    creation_time = creation_time.strftime("%Y-%m-%d %H:%M:%S")
+                    archive_time = archive_time.strftime("%Y-%m-%d %H:%M:%S")
+                    await thread.send(f"@everyone\n Created : {creation_time} (UTC+9)\nArchived : {archive_time} (UTC+9).")
+                    logger.info(f'Thread archive time : {archive_time} (UTC+9)')
+        return bot
